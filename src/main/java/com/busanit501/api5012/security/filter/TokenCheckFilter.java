28c1b9d36941a6322df73d5cfb81b58286e73775
이상용 <lsy3709@naver.com>
2025-02-04 09:52:12 +0900

    1교시 , 토큰 검사 필터 추가

--- src/main/java/com/busanit501/api5012/security/filter/TokenCheckFilter.java
+++ src/main/java/com/busanit501/api5012/security/filter/TokenCheckFilter.java
@@ -1,6 +1,10 @@
 package com.busanit501.api5012.security.filter;
 
+import com.busanit501.api5012.security.exception.AccessTokenException;
 import com.busanit501.api5012.util.JWTUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,6 +14,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
@@ -40,5 +45,41 @@ public class TokenCheckFilter extends OncePerRequestFilter {
 
         // 다음 필터로 요청 전달
         filterChain.doFilter(request, response);
+    }
+
+    //토큰 검사하는 도구.
+    public Map<String, Object> validateAccessToken(HttpServletRequest request) throws AccessTokenException {
+        String headerStr = request.getHeader("Authorization");
+
+        // 1. Authorization 헤더가 없는 경우
+        if (headerStr == null || headerStr.length() < 8) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.UNACCEPT);
+        }
+
+        // 2. 토큰 타입 확인
+        String tokenType = headerStr.substring(0, 6);
+        String tokenStr = headerStr.substring(7);
+
+        if (!tokenType.equalsIgnoreCase("Bearer")) {
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADTYPE);
+        }
+
+        try {
+            // 3. JWT 검증
+            Map<String, Object> values = jwtUtil.validateToken(tokenStr);
+            return values;
+
+        } catch (MalformedJwtException malformedJwtException) {
+            log.error("MalformedJwtException: Invalid token format.");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.MALFORM);
+
+        } catch (SignatureException signatureException) {
+            log.error("SignatureException: Invalid token signature.");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.BADSIGN);
+
+        } catch (ExpiredJwtException expiredJwtException) {
+            log.error("ExpiredJwtException: Token has expired.");
+            throw new AccessTokenException(AccessTokenException.TOKEN_ERROR.EXPIRED);
+        }
     }
 }
